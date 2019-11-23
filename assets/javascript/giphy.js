f7651719b51dd55ff3c51d6038b2a517f55646de
Francisco Fernandez <francisco.a.fernandez@gmail.com>
2019-11-22 22:27:43 -0500

    pseudo coded functions for new button,remove button and gif display

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 
     // array of cars
-    var hotcars = ["Volvo S60R", "Audi RS-6", "Toyota Supra", "Ford Mustang GT", "Aston Martin Vulcan", "BMW M5", "McLaren P9", "Koenigsegg CCX"];
+    var hotcars = ["Volvo S60-R", "Audi RS-6", "Toyota Supra", "Ford Mustang GT", "Aston Martin Vulcan", "BMW M5", "McLaren P9", "Koenigsegg CCX"];
 
 
     //function that displays the gif buttons
     function displayGifButtons() {
-        $("#gifButtonsView").empty();
+        $("#giphyButtonsView").empty();
         for (var i = 0; i < hotcars.length; i++) {
             var gifButton = $("<button>");
             gifButton.addClass("cars");
             gifButton.addClass("btn btn-primary")
             gifButton.attr("data-name", hotcars[i]);
             gifButton.text(hotcars[i]);
-            $("#gifButtonsView").append(gifButton);
+            $("#giphyButtonsView").append(giphyButton);
         }
     }
 })
+
+//function that adds new button
+
+
+
+
+//function that removes last button
+
+
+
+//function that displays gifs
